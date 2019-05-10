919431da27e9552e4fd23dcc519a39547442aa7c
Nicholas Schafer <npschafer@gmail.com>
2019-05-10 09:26:39 -0500

    finished

--- school.py
+++ school.py
@@ -1,1 +1,16 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+    def add_student(self, name, grade):
+        if grade in self.roster.keys():
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+    def grade(self, grade):
+        return self.roster[grade]
+    def sort_roster(self):
+        sorted_roster = {}
+        for grade, class_list in self.roster.items():
+            sorted_roster[grade] = sorted(class_list)
+        return sorted_roster
